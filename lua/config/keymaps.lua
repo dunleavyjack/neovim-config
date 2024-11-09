5c4145f5d819b4914a6aaa8a507756250dc30be6
dunleavyjack <dunleavyjack@gmail.com>
2024-11-08 20:12:43 -0600

    feat(custom-keymaps): added custom keymap for jumping to end of line

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,5 +3,5 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "+", "$", {
-  desc = "Jump to after last character in line, except the key is next to the character for jumping to after the first character in line",
+  desc = "Jump after last character in line, except the key is next to the character for jumping to after the first character in line",
 })
